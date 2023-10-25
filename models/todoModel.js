e2de25b34f06b8dfcd263fa6a110b1965a4bed00
Poodoop <dickyaristyo43@gmail.com>
2023-10-25 22:23:51 +0700

    feat: update model

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,1 +1,50 @@
 const pool = require('../config/connection.js')
+
+class todo {
+    static async getTodo() {
+        try {
+            const results = await pool.query(
+                'SELECT * FROM todo'
+            )
+            return results.rows
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async detailTodo(todoId) {
+        try {
+            const results = await pool.query(
+                'SELECT * FROM todo WHERE id = $1',
+                [todoId]
+            )
+            return results.rows
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async createTodo(title, id) {
+        try {
+            await pool.query(
+                'INSERT INTO todo ("title", "id") VALUES ($1, $2)',
+                [title, id]
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async deleteTodo(todoId) {
+        try {
+            await pool.query(
+                'DELETE FROM todo WHERE id = $1',
+                [todoId]
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+module.exports = todo;
