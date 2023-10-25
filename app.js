3c9128809939c662517406adfac514851639fa11
Poodoop <dickyaristyo43@gmail.com>
2023-10-25 20:46:06 +0700

    chore: change var to const

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-var app = express()
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var todo = require('./routes/todo.js')
+const todo = require('./routes/todo.js')
 app.use('/todo', todo)
 
 app.listen(3000)
